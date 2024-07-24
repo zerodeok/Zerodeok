660ddd335c93dcfce73448e29922214e948f6af8
zerodeok <dudejr3165@naver.com>
2024-07-25 02:31:29 +0900

    remove 동작

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,14 +1,14 @@
 package calculator;
 
 import java.sql.SQLOutput;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] inquiry = new int[10];
         int index = 0;
+        LinkedList<Integer> inquiry = new LinkedList<>();
         while(true) {
             System.out.println("양의 정수 2개를 입력해주세요");
             System.out.println("첫 번째 숫자를 입력하세요: ");
@@ -18,78 +18,67 @@ public class App {
 
             System.out.println("사칙연산 기호를 입력하세요: ");
             char ch = sc.next().charAt(0);
+            if(!(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch =='%')){
+                System.out.println("사칙연산 기호를 잘못 입력하였습니다.");
+                return;
+            }
             int result =0;
+
             switch (ch) {
                 case '+':
                     result = firstInt + secondInt;
                     System.out.println(result);
-                    inquiry[index] = result;
-                    index++;
-                    if(index == 9){
-                        for(int i = 0;i<10;i++){
-                            inquiry[i] = inquiry[i+1];
-                        }
-                    }
+                    inquiry.add(index,result);
+
                     break;
 
                 case '-':
                     result = firstInt - secondInt;
                     System.out.println(result);
-                    inquiry[index] = result;
-                    index++;
-                    if(index == 9){
-                        for(int i = 0;i<10;i++){
-                            inquiry[i] = inquiry[i+1];
-                        }
-                    }
+                    inquiry.add(index,result);
                     break;
 
                 case '*':
                     result = firstInt * secondInt;
                     System.out.println(result);
-                    inquiry[index] = result;
-                    index++;
-                    if(index == 9){
-                        for(int i = 0;i<10;i++){
-                            inquiry[i] = inquiry[i+1];
-                        }
-                    }
+                    inquiry.add(index,result);
+
                     break;
 
                 case '/':
+
                     if (secondInt > 0) {
                         result = firstInt / secondInt;
                         System.out.println(result);
-                        inquiry[index] = result;
-                        index++;
+                        inquiry.add(index,result);
                     } else {
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     }
-                    if(index == 9){
-                        for(int i = 0;i<10;i++){
-                            inquiry[i] = inquiry[i+1];
-                        }
-                    }
+
                     break;
 
                 case '%':
                     result = firstInt % secondInt;
                     System.out.println("결과: "+result);
-                    inquiry[index] = result;
-                    index++;
-                    if(index == 9){
-                        for(int i = 0;i<10;i++){
-                            inquiry[i] = inquiry[i+1];
-                        }
-                    }
+                    inquiry.add(index,result);
                     break;
             }
+            index++;
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String inquiryStr = sc.next();
+
             if(inquiryStr.equals("inquiry")){
-                for( int i : inquiry ){
+                for( Integer i : inquiry ){
                     System.out.println(i);
                 }
+            }
+
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String removeStr = sc.next();
+            if(removeStr.equals("remove")){
+                inquiry.remove(0);
+                index--;
             }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exitStr = sc.next();
