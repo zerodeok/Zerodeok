002ca84f8e13560fd2032431c7b6b1d96aea3707
zerodeok <dudejr3165@naver.com>
2024-07-24 19:03:01 +0900

    문자입력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,22 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("테스트");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("양의 정수 2개를 입력해주세요");
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int firstInt = sc.nextInt();
+        System.out.println("두번째 숫자를 입력해주세요: ");
+        int secondInt = sc.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        char ch = sc.next().charAt(0);
+
+
+
+
     }
 }
